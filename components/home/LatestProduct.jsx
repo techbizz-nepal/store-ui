33cf56392861d09926c744df78f2bb60328c3e36
Susant Paudel <techbizznepal@gmail.com>
2023-03-27 23:38:27 +0545

    component shifting done

---
+++ components/home/LatestProduct.jsx
@@ -0,0 +1,43 @@
+import {Card, Grid, Row, Text} from "@nextui-org/react";
+import React from "react";
+
+const LatestProduct = ({latestProducts}) => {
+
+    return (
+        <div className={`containerDiv px-4 flex flex-col`}>
+            <div className={`flex items-center justify-between px-4`}>
+                <div><Text h2>Latest Deals</Text></div>
+                <div className={`text-sm hover:text-secondary-600 text-black cursor-pointer select-none`}>View All</div>
+            </div>
+            <div>
+                <Grid.Container gap={2} justify="flex-start">
+                    {latestProducts.map((item, index) => (
+                        <Grid xs={6} sm={3} key={index}>
+                            <Card isPressable>
+                                <Card.Body css={{p: 0}}>
+                                    <Card.Image
+                                        src={"https://nextui.org" + item.img}
+                                        objectFit="cover"
+                                        width="100%"
+                                        height={140}
+                                        alt={item.title}
+                                    />
+                                </Card.Body>
+                                <Card.Footer css={{justifyItems: "flex-start"}}>
+                                    <Row wrap="wrap" justify="space-between" align="center">
+                                        <Text b>{item.title}</Text>
+                                        <Text css={{color: "$accents7", fontWeight: "$semibold", fontSize: "$sm"}}>
+                                            {item.price}
+                                        </Text>
+                                    </Row>
+                                </Card.Footer>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid.Container>
+            </div>
+        </div>
+    )
+}
+
+export default LatestProduct
--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,12 @@
 import Layout from "../components/Layout";
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import Head from "next/head";
 import HeroSlider from "../components/home/HeroSlider";
 import useSliderAction from "../hooks/useSliderAction";
-import {Card, Grid, Link, Row, Spacer, Text} from "@nextui-org/react";
+import {Spacer} from "@nextui-org/react";
+import LatestProduct from "../components/home/LatestProduct";
+import TopProduct from "../components/home/TopProduct";
 
-const LatestProduct = ({latestProducts}) => {
-
-    return (
-        <div className={`containerDiv px-4 flex flex-col`}>
-            <div className={`flex items-center justify-between px-4`}>
-                <div><Text h2>Latest Deals</Text></div>
-                <div className={`text-sm hover:text-secondary-600 text-black cursor-pointer select-none`}>View All</div>
-            </div>
-            <div>
-                <Grid.Container gap={2} justify="flex-start">
-                    {latestProducts.map((item, index) => (
-                        <Grid xs={6} sm={3} key={index}>
-                            <Card isPressable>
-                                <Card.Body css={{p: 0}}>
-                                    <Card.Image
-                                        src={"https://nextui.org" + item.img}
-                                        objectFit="cover"
-                                        width="100%"
-                                        height={140}
-                                        alt={item.title}
-                                    />
-                                </Card.Body>
-                                <Card.Footer css={{justifyItems: "flex-start"}}>
-                                    <Row wrap="wrap" justify="space-between" align="center">
-                                        <Text b>{item.title}</Text>
-                                        <Text css={{color: "$accents7", fontWeight: "$semibold", fontSize: "$sm"}}>
-                                            {item.price}
-                                        </Text>
-                                    </Row>
-                                </Card.Footer>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid.Container>
-            </div>
-        </div>
-    )
-}
-const TopProduct = ({topProducts}) => {
-
-    return (
-        <div className={`containerDiv px-4 flex flex-col`}>
-            <div className={`flex items-center justify-between px-4`}>
-                <div><Text h2>Top Products</Text></div>
-                <div className={`text-sm hover:text-secondary-600 text-black cursor-pointer select-none`}>View All</div>
-            </div>
-            <div>
-                <Grid.Container gap={2} justify="flex-start">
-                    {topProducts.map((item, index) => (
-                        <Grid xs={6} sm={3} key={index}>
-                            <Card isPressable>
-                                <Card.Body css={{p: 0}}>
-                                    <Card.Image
-                                        src={"https://nextui.org" + item.img}
-                                        objectFit="cover"
-                                        width="100%"
-                                        height={140}
-                                        alt={item.title}
-                                    />
-                                </Card.Body>
-                                <Card.Footer css={{justifyItems: "flex-start"}}>
-                                    <Row wrap="wrap" justify="space-between" align="center">
-                                        <Text b>{item.title}</Text>
-                                        <Text css={{color: "$accents7", fontWeight: "$semibold", fontSize: "$sm"}}>
-                                            {item.price}
-                                        </Text>
-                                    </Row>
-                                </Card.Footer>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid.Container>
-            </div>
-        </div>
-    )
-}
 const Home = ({serverData, error}) => {
     const {currentIndex, slides, prevSlide, nextSlide, goToSlide} = useSliderAction()
     const [latestProducts, setLatestProducts] = useState([
@@ -125,11 +51,6 @@ const Home = ({serverData, error}) => {
             price: "$12.20",
         },
     ])
-
-    useEffect(() => {
-        const interval = setInterval(() => nextSlide(), 10000)
-        return () => clearInterval(interval)
-    }, [currentIndex, nextSlide])
 
     if (error) {
         return <h1>Error Occurred</h1>
