66f571dfc607ab89b4d453e614021b43b0c49fb8
Susant Paudel <susanta.paudel@logicinfo.com>
2023-03-31 09:37:50 +0545

    some design work

--- pages/[abstractId].jsx
+++ pages/[abstractId].jsx
@@ -10,35 +10,46 @@ import {FaShoppingCart} from "react-icons/fa";
 const ProductDetail = ({productDetail}) => {
     const [quantity, setQuantity] = useState(1);
 
-    return <div id={`content`}
-                className={`flex justify-start gap-x-4 containerDiv`}>
-        <div id={`image`} className={`w-full relative basis-2/3`}>
-            <Image
-                src={productDetail.imageSrc}
-                width={1440} height={764}/>
-        </div>
-        <div id={`detailAction`} className={`flex flex-col gap-y-10 basis-1/3 px-2`}>
-            <div id={`title`} className={`text-3xl font-medium`}>{productDetail.pageName}</div>
-            <div id={`price`} className="text-7xl font-semibold">$10</div>
-            <div id={`quantity`} className={`flex flex-col gap-y-4`}>
-                <div id={`quantityLabel`} className={`text-lg font-extralight`}>Quantity</div>
-                <div id={`quantityAction`} className="flex gap-x-6 items-center text-2xl">
-                    <div
-                        onClick={() => setQuantity(prevState => prevState !== 0 ? prevState - 1 : prevState)}
-                        className={`flex px-2 font-semibold text-action-600 select-none cursor-pointer py-1 w-10 h-10 border border-action-300 rounded-full items-center justify-center`}>-
+    return (
+        <div id={`content`} className={`flex flex-col gap-y-10 items-center`}>
+            <div id={`detailAction`}
+                 className={`containerDiv flex justify-start gap-x-4`}>
+                <div id={`image`} className={`w-full relative basis-2/3`}>
+                    <Image
+                        src={productDetail.imageSrc}
+                        width={1440} height={764}/>
+                </div>
+                <div id={`detailAction`} className={`flex flex-col gap-y-10 basis-1/3 px-2`}>
+                    <div id={`title`} className={`text-3xl font-medium`}>{productDetail.pageName}</div>
+                    <div id={`price`} className="text-7xl font-semibold">$10</div>
+                    <div id={`quantity`} className={`flex flex-col gap-y-4`}>
+                        <div id={`quantityLabel`} className={`text-lg font-extralight`}>Quantity</div>
+                        <div id={`quantityAction`} className="flex gap-x-6 items-center text-2xl">
+                            <div
+                                onClick={() => setQuantity(prevState => prevState !== 0 ? prevState - 1 : prevState)}
+                                className={`flex px-2 font-semibold text-action-600 select-none cursor-pointer py-1 w-10 h-10 border border-action-300 rounded-full items-center justify-center`}>-
+                            </div>
+                            <div className={`flex items-center justify-center w-6`}>{quantity}</div>
+                            <div
+                                onClick={() => setQuantity(prevState => prevState >= 0 ? quantity + 1 : prevState)}
+                                className={`flex px-2 font-semibold text-action-600 select-none cursor-pointer py-1 w-10 h-10 border border-action-300 rounded-full items-center justify-center`}>+
+                            </div>
+                        </div>
                     </div>
-                    <div className={`flex items-center justify-center w-6`}>{quantity}</div>
-                    <div
-                        onClick={() => setQuantity(prevState => prevState >= 0 ? quantity + 1 : prevState)}
-                        className={`flex px-2 font-semibold text-action-600 select-none cursor-pointer py-1 w-10 h-10 border border-action-300 rounded-full items-center justify-center`}>+
+                    <div id={`addToCartBtn`}>
+                        <Button size={`lg`} icon={<FaShoppingCart className={`text-white`}/>}>Add to Cart</Button>
                     </div>
                 </div>
             </div>
-            <div id={`addToCartBtn`}>
-                <Button size={`lg`} icon={<FaShoppingCart className={`text-white`}/>}>Add to Cart</Button>
+            <div id={`detailBody`} className={`containerDiv flex justify-start gap-x-4`}>
+                <div className={`flex-auto flex-col`}>
+                    <div id={`title`} className={`text-md font-light`}>Description</div>
+                    <div id={`body`}>Body</div>
+                </div>
+                <div className={`flex-auto`}>detail</div>
             </div>
         </div>
-    </div>
+    )
 }
 const AbstractProductDetail = () => {
     const router = useRouter()
